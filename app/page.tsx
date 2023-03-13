71f3d06256e1a222e2883ab9fbd69f39166fb79b
Asif Raza <asif6903603@gmail.com>
2023-03-13 16:01:03 +0500

    portfolio website using Next.js with Tailwind CSS

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+import About from "./About";
+import Basic from "./Basic";
+import Footer from "./Footer";
+import Header from "./Header";
+import Project from "./Project";
+
+
+
+
+export default function Home() {
+  return (
+<>
+<Header />
+<Basic/>
+<About />
+ <Project />  
+<Footer />
+</>
+  )
+}
